6c9f1e2a9fe978cc2a87cd15da86123347d46253
Samiul Hasan <samiulxhasan650@gmail.com>
2022-04-04 07:48:12 +0600

    all reviews page done

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,21 @@
-// import useReviews from '../../hook/useReviews';
-
+import { Container, Row } from "react-bootstrap";
+import useReviews from "../../hook/useReviews";
+import SingleReview from "../SingleReview/SingleReview";
+import './Reviews.css';
 const Reviews = () => {
-    // const [reviews, setReviews] = useReviews();
+
+    const [reviews, setReviews] = useReviews();
     return (
-        <div>
-            <h2>Reviews Page</h2>
+        <div className="all-reviews py-5">
+            <h2 className='text-center mb-5'>Customer Reviews</h2>
+            <Container>
+                <Row xs={1} md={3} className="g-5 mb-5">
+                    {
+                        reviews.map(review => <SingleReview key={review._id} review={review}></SingleReview>)
+                    }
+                </Row>
+            </Container>
+
         </div>
     );
 };
