9f9773bd31ac09d388b35d6681d0a2d6196cdebe
Samiul Hasan <samiulxhasan650@gmail.com>
2022-04-05 02:57:01 +0600

    finalizing left

---
+++ src/components/BikeSpec/BikeSpec.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Col, Container, Row, Table } from 'react-bootstrap';
+
+const BikeSpec = () => {
+    const img ='https://raw.githubusercontent.com/SamiulxHasanx07/pro-moto-fake-data/main/kawasaki-h2r-2.jpg';
+    return (
+        <div div className= 'bike-spec py-5' >
+            <Container className='bike-spec py-5'>
+                <h2 className="text-center mb-5">Ninja H2R Spec</h2>
+                <Row>
+                    <Col sm='12' md='6'>
+                        <img className='img-fluid p-5' src={img} alt="ninja-spec" />
+                    </Col>
+                    <Col sm='12' md='6'>
+                        <h2 style={{ color: "#47c335" }}>Ninja H2R Spec</h2>
+                        <div>
+                            <Table className='table-borderless'>
+                                <tr>
+                                    <td>Engine:</td>
+                                    <td>4-stroke,16-valve</td>
+                                </tr>
+                                <tr>
+                                    <td>Displacement:</td>
+                                    <td>998cc</td>
+                                </tr>
+                                <tr>
+                                    <td>Torque:</td>
+                                    <td>121.5 lb-ft @ 12,500 rpm</td>
+                                </tr>
+                                <tr>
+                                    <td>Ignition:</td>
+                                    <td>Digital</td>
+                                </tr>
+                                <tr>
+                                    <td>Front Tire:</td>
+                                    <td>120/60 ZR17 V01F slick</td>
+                                </tr>
+                                <tr>
+                                    <td>Rear Tire:</td>
+                                    <td>190/65 17</td>
+                                </tr>
+                                <tr>
+                                    <td>Rear Brakes:</td>
+                                    <td>single 250mm disc, KIBS ABS</td>
+                                </tr>
+                                <tr>
+                                    <td>Frame Type</td>
+                                    <td>Trellis, high-tensile steel</td>
+                                </tr>
+                                <tr>
+                                    <td>Length</td>
+                                    <td>81.5 in</td>
+                                </tr>
+                                <tr>
+                                    <td>Width</td>
+                                    <td>33.5 in</td>
+                                </tr>
+                                <tr>
+                                    <td>Height</td>
+                                    <td>45.7 in</td>
+                                </tr>
+                                <tr>
+                                    <td>Curb Weight</td>
+                                    <td>476.3 lb*</td>
+                                </tr>
+                                <tr>
+                                    <td>Fuel Capacity</td>
+                                    <td>4.5 gal</td>
+                                </tr>
+                            </Table>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+    </div>
+    );
+};
+
+export default BikeSpec;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Col, Container, Row, Button, Table } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import './Home.css';
 import useReviews from '../../hook/useReviews';
 import SingleReview from '../SingleReview/SingleReview';
 import { useNavigate } from "react-router-dom";
+import BikeSpec from '../BikeSpec/BikeSpec';
 const Home = () => {
     // fake data
     const bikeInfo = {
@@ -13,9 +14,8 @@ const Home = () => {
         torque: '165 Nm',
         power: '322 bhp',
         img: 'https://raw.githubusercontent.com/SamiulxHasanx07/pro-moto-fake-data/main/kawasaki-h2r.png',
-        img2: 'https://raw.githubusercontent.com/SamiulxHasanx07/pro-moto-fake-data/main/kawasaki-h2r-2.jpg'
     }
-    const { name, description, cc, torque, power, img, img2 } = bikeInfo;
+    const { name, description, cc, torque, power, img } = bikeInfo;
 
     const [reviews, setReviews] = useReviews();
     const newData = reviews.slice(0, 3);
@@ -76,77 +76,7 @@ const Home = () => {
                     </div>
                 </Container>
             </div>
-
-            {/* bike specification section */}
-            <div className='bike-spec py-5'>
-                <Container className='bike-spec py-5'>
-                    <h2 className="text-center mb-5">Ninja H2R Spec</h2>
-                    <Row>
-                        <Col sm='12' md='6'>
-                            <img className='img-fluid p-5' src={img2} alt="ninja-spec" />
-                        </Col>
-                        <Col sm='12' md='6'>
-                            <h2 style={{color:"#47c335"}}>Ninja H2R Spec</h2>
-                            <div>
-                                <Table className='table-borderless'>
-                                    <tr>
-                                        <td>Engine:</td>
-                                        <td>4-stroke,16-valve</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Displacement:</td>
-                                        <td>998cc</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Torque:</td>
-                                        <td>121.5 lb-ft @ 12,500 rpm</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Ignition:</td>
-                                        <td>Digital</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Front Tire:</td>
-                                        <td>120/60 ZR17 V01F slick</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Rear Tire:</td>
-                                        <td>190/65 17</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Rear Brakes:</td>
-                                        <td>single 250mm disc, KIBS ABS</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Frame Type</td>
-                                        <td>Trellis, high-tensile steel</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Length</td>
-                                        <td>81.5 in</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Width</td>
-                                        <td>33.5 in</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Height</td>
-                                        <td>45.7 in</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Curb Weight</td>
-                                        <td>476.3 lb*</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Fuel Capacity</td>
-                                        <td>4.5 gal</td>
-                                    </tr>
-                                </Table>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <BikeSpec></BikeSpec>
         </>
     );
 };
